77ed66acb3c546ae2e111a068d4c5b45d96d1f58
sumant-007 <sumant@altisec.in>
2024-09-11 17:12:03 +0530

    second commit

--- src/studyopedia/Demo.java
+++ src/studyopedia/Demo.java
@@ -5,6 +5,7 @@ public class Demo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("First Java program.......");
+		System.out.println("Second Java program.......");
 	}
 
 }
