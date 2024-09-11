47fa8380269f6f1faa1b2860c29f5fd3869410d2
sumant-007 <sumant@altisec.in>
2024-09-11 16:53:27 +0530

    first commit

---
+++ src/studyopedia/Demo.java
@@ -0,0 +1,10 @@
+package studyopedia;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("First Java program.......");
+	}
+
+}
